1ef3ed771b8b77afbfbf5b613374bd6d5a10798d
aahlaqqa <aahlaqqa@c3r1p1.1337.ma>
2025-02-15 11:42:40 +0100

    ahmed

--- mendatory/move_player.c
+++ mendatory/move_player.c
@@ -6,13 +6,13 @@
 /*   By: aahlaqqa <aahlaqqa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 14:35:21 by ahmed             #+#    #+#             */
-/*   Updated: 2025/02/15 11:28:22 by aahlaqqa         ###   ########.fr       */
+/*   Updated: 2025/02/15 11:42:24 by aahlaqqa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3D.h"
 
-void	check_bounds(t_data *data)
+void check_bounds(t_data *data)
 {
 	if (data->mini_map[(int)data->new_y][(int)data->new_x] == '0')
 	{
@@ -23,7 +23,7 @@ void	check_bounds(t_data *data)
 	}
 }
 
-void	key_movement(t_data *data, int keycode)
+void key_movement(t_data *data, int keycode)
 {
 	if (keycode == 115)
 		data->new_x--;
@@ -35,12 +35,9 @@ void	key_movement(t_data *data, int keycode)
 		data->new_y++;
 }
 
-void	check_if_condition(t_data *data)
+void check_if_condition(t_data *data)
 {
-	if (data->mini_map[data->player_i][data->player_j] == 'N'
-		|| data->mini_map[data->player_i][data->player_j] == 'W'
-		|| data->mini_map[data->player_i][data->player_j] == 'S'
-		|| data->mini_map[data->player_i][data->player_j] == 'E')
+	if (data->mini_map[data->player_i][data->player_j] == 'N' || data->mini_map[data->player_i][data->player_j] == 'W' || data->mini_map[data->player_i][data->player_j] == 'S' || data->mini_map[data->player_i][data->player_j] == 'E')
 	{
 		data->player_y = (double)data->player_i;
 		data->player_x = (double)data->player_j;
@@ -48,35 +45,41 @@ void	check_if_condition(t_data *data)
 }
 int move_player(t_data *data, int keycode)
 {
-    data->player_i = 0;
-    while (data->mini_map[data->player_i])
-    {
-        data->player_j = 0;
-        while (data->mini_map[data->player_i][data->player_j])
-        {
-            check_if_condition(data);
-            data->player_j++;
-        }
-        data->player_i++;
-    }
-    data->new_x = data->player_x;
-    data->new_y = data->player_y;
-    key_movement(data, keycode);
-    if (data->new_x >= 0 && data->new_x < data->map_width &&
-        data->new_y >= 0 && data->new_y < data->map_height &&
-        data->mini_map[(int)data->new_y][(int)data->new_x] == '0')
-    {
-        data->mini_map[(int)data->player_y][(int)data->player_x] = '0';
-        data->mini_map[(int)data->new_y][(int)data->new_x] = 'N';
-        data->player_x = data->new_x;
-        data->player_y = data->new_y;
-    }
+	data->player_i = 0;
+	while (data->mini_map[data->player_i])
+	{
+		data->player_j = 0;
+		while (data->mini_map[data->player_i][data->player_j])
+		{
+			check_if_condition(data);
+			data->player_j++;
+		}
+		data->player_i++;
+	}
+	data->new_x = data->player_x;
+	data->new_y = data->player_y;
+	key_movement(data, keycode);
+
+	if (data->new_x >= 0 && data->new_x < data->map_width &&
+		data->new_y >= 0 && data->new_y < data->map_height &&
+		data->mini_map[(int)data->new_y][(int)data->new_x] == '0')
+	{
+		data->mini_map[(int)data->player_y][(int)data->player_x] = '0';
+		if (data->dir_y < -0.5)
+			data->mini_map[(int)data->new_y][(int)data->new_x] = 'N';
+		else if (data->dir_y > 0.5)
+			data->mini_map[(int)data->new_y][(int)data->new_x] = 'S';
+		else if (data->dir_x < -0.5)
+			data->mini_map[(int)data->new_y][(int)data->new_x] = 'W';
+		else if (data->dir_x > 0.5)
+			data->mini_map[(int)data->new_y][(int)data->new_x] = 'E';
+		data->player_x = data->new_x;
+		data->player_y = data->new_y;
+	}
 	raycasting(data);
-    draw_mini_map(data);
-    return (0);
+	draw_mini_map(data);
+	return (0);
 }
-
-
 
 // int	press_key(int keycode, t_data *data)
 // {
