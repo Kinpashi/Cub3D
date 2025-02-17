b28f5f988940b418af7b0696aa2819b0dd1d1f33
aahlaqqa <aahlaqqa@e1r1p3.1337.ma>
2025-02-17 16:17:20 +0100

    ahmed

--- mendatory/cube_movements.c
+++ mendatory/cube_movements.c
@@ -6,7 +6,7 @@
 /*   By: aahlaqqa <aahlaqqa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 14:45:20 by aahlaqqa          #+#    #+#             */
-/*   Updated: 2025/02/17 15:41:26 by aahlaqqa         ###   ########.fr       */
+/*   Updated: 2025/02/17 16:15:21 by aahlaqqa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,31 +14,37 @@
 
 int press_key(int key, t_data *data)
 {
-    printf("Current tile: %c\n", data->mini_map[(int)data->player_x][(int)data->player_y]);
-    printf("Tile ahead: %c\n", data->mini_map[(int)(data->player_x + data->dir_x * MOVE_SPEED)][(int)data->player_y]);
-    printf("Tile right: %c\n", data->mini_map[(int)data->player_x][(int)(data->player_y + data->dir_y * MOVE_SPEED)]);
     if (key == 65307) // ESC key
         exit(0);
-    if (key == 119) // W Key
+    if (key == 119) // W up
     {
-        printf("Moving forward (W)\n");
-        printf("Before movement: x = %f, y = %f\n", data->player_x, data->player_y);
-
         if (data->mini_map[(int)(data->player_x + data->dir_x * MOVE_SPEED)][(int)data->player_y] != '1')
             data->player_x += data->dir_x * MOVE_SPEED;
 
         if (data->mini_map[(int)data->player_x][(int)(data->player_y + data->dir_y * MOVE_SPEED)] != '1')
             data->player_y += data->dir_y * MOVE_SPEED;
-
-        printf("After movement: x = %f, y = %f\n", data->player_x, data->player_y);
     }
-    else if (key == 115) // S Key
+    else if (key == 115) // S down
     {
         if (data->mini_map[(int)(data->player_x - data->dir_x * MOVE_SPEED)][(int)data->player_y] != '1')
             data->player_x -= data->dir_x * MOVE_SPEED;
 
         if (data->mini_map[(int)data->player_x][(int)(data->player_y - data->dir_y * MOVE_SPEED)] != '1')
             data->player_y -= data->dir_y * MOVE_SPEED;
+    }
+    else if (key == 97) // A left
+    {
+        if (data->mini_map[(int)(data->player_x - data->plan_x * MOVE_SPEED)][(int)data->player_y] != '1')
+            data->player_x -= data->plan_x * MOVE_SPEED;
+        if (data->mini_map[(int)data->player_x][(int)(data->player_y - data->plan_y * MOVE_SPEED)] != '1')
+            data->player_y -= data->plan_y * MOVE_SPEED;
+    }
+    else if (key == 100) // D right
+    {
+        if (data->mini_map[(int)(data->player_x + data->plan_x * MOVE_SPEED)][(int)data->player_y] != '1')
+            data->player_x += data->plan_x * MOVE_SPEED;
+        if (data->mini_map[(int)data->player_x][(int)(data->player_y + data->plan_y * MOVE_SPEED)] != '1')
+            data->player_y += data->plan_y * MOVE_SPEED;
     }
     raycasting(data);
     return (0);
--- mendatory/move_player.c
+++ mendatory/move_player.c
@@ -6,7 +6,7 @@
 /*   By: aahlaqqa <aahlaqqa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 14:35:21 by ahmed             #+#    #+#             */
-/*   Updated: 2025/02/17 15:29:16 by aahlaqqa         ###   ########.fr       */
+/*   Updated: 2025/02/17 15:46:39 by aahlaqqa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,58 +23,58 @@ void check_bounds(t_data *data)
 	}
 }
 
-void key_movement(t_data *data, int keycode)
-{
-	if (keycode == 115)
-		data->new_x--;
-	else if (keycode == 119)
-		data->new_x++;
-	else if (keycode == 95)
-		data->new_y--;
-	else if (keycode == 100)
-		data->new_y++;
-}
+// void key_movement(t_data *data, int keycode)
+// {
+// 	if (keycode == 115)
+// 		data->new_x--;
+// 	else if (keycode == 119)
+// 		data->new_x++;
+// 	else if (keycode == 95)
+// 		data->new_y--;
+// 	else if (keycode == 100)
+// 		data->new_y++;
+// }
 
-void check_if_condition(t_data *data)
-{
-	if (data->mini_map[data->player_i][data->player_j] == 'N' || data->mini_map[data->player_i][data->player_j] == 'W' || data->mini_map[data->player_i][data->player_j] == 'S' || data->mini_map[data->player_i][data->player_j] == 'E')
-	{
-		data->player_y = (double)data->player_i;
-		data->player_x = (double)data->player_j;
-	}
-}
-int move_player(t_data *data, int keycode)
-{
-	data->player_i = 0;
-	while (data->mini_map[data->player_i])
-	{
-		data->player_j = 0;
-		while (data->mini_map[data->player_i][data->player_j])
-		{
-			check_if_condition(data);
-			data->player_j++;
-		}
-		data->player_i++;
-	}
-	data->new_x = data->player_x;
-	data->new_y = data->player_y;
-	key_movement(data, keycode);
-	if (data->new_x >= 0 && data->new_x < data->map_width &&
-		data->new_y >= 0 && data->new_y < data->map_height &&
-		data->mini_map[(int)data->new_y][(int)data->new_x] == '0')
-	{
-		data->mini_map[(int)data->player_y][(int)data->player_x] = '0';
-		if (data->dir_y < -0.5)
-			data->mini_map[(int)data->new_y][(int)data->new_x] = 'N';
-		else if (data->dir_y > 0.5)
-			data->mini_map[(int)data->new_y][(int)data->new_x] = 'S';
-		else if (data->dir_x < -0.5)
-			data->mini_map[(int)data->new_y][(int)data->new_x] = 'W';
-		else if (data->dir_x > 0.5)
-			data->mini_map[(int)data->new_y][(int)data->new_x] = 'E';
-		data->player_x = data->new_x;
-		data->player_y = data->new_y;
-	}
-	raycasting(data);
-	return (0);
-}
+// void check_if_condition(t_data *data)
+// {
+// 	if (data->mini_map[data->player_i][data->player_j] == 'N' || data->mini_map[data->player_i][data->player_j] == 'W' || data->mini_map[data->player_i][data->player_j] == 'S' || data->mini_map[data->player_i][data->player_j] == 'E')
+// 	{
+// 		data->player_y = (double)data->player_i;
+// 		data->player_x = (double)data->player_j;
+// 	}
+// }
+// int move_player(t_data *data, int keycode)
+// {
+// 	data->player_i = 0;
+// 	while (data->mini_map[data->player_i])
+// 	{
+// 		data->player_j = 0;
+// 		while (data->mini_map[data->player_i][data->player_j])
+// 		{
+// 			check_if_condition(data);
+// 			data->player_j++;
+// 		}
+// 		data->player_i++;
+// 	}
+// 	data->new_x = data->player_x;
+// 	data->new_y = data->player_y;
+// 	key_movement(data, keycode);
+// 	if (data->new_x >= 0 && data->new_x < data->map_width &&
+// 		data->new_y >= 0 && data->new_y < data->map_height &&
+// 		data->mini_map[(int)data->new_y][(int)data->new_x] == '0')
+// 	{
+// 		data->mini_map[(int)data->player_y][(int)data->player_x] = '0';
+// 		if (data->dir_y < -0.5)
+// 			data->mini_map[(int)data->new_y][(int)data->new_x] = 'N';
+// 		else if (data->dir_y > 0.5)
+// 			data->mini_map[(int)data->new_y][(int)data->new_x] = 'S';
+// 		else if (data->dir_x < -0.5)
+// 			data->mini_map[(int)data->new_y][(int)data->new_x] = 'W';
+// 		else if (data->dir_x > 0.5)
+// 			data->mini_map[(int)data->new_y][(int)data->new_x] = 'E';
+// 		data->player_x = data->new_x;
+// 		data->player_y = data->new_y;
+// 	}
+// 	raycasting(data);
+// 	return (0);
+// }
